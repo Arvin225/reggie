3007d0c64f1a900a427d7852dbd0a361aca21386
Arvin <60811406+Arvin225@users.noreply.github.com>
2023-02-21 16:48:40 +0800

    redis缓存优化：手机登录验证码

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -9,6 +9,7 @@ import com.itheima.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -24,6 +26,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 发送短信验证码
@@ -36,13 +41,22 @@ public class UserController {
     public R<Object> sendMsg(@RequestBody User user, HttpServletRequest request) {
         String phone = user.getPhone();
         log.info("接收到 {} 的短信验证码发送请求", phone);
+
         if (StringUtils.isEmpty(phone)) {
             return R.error("验证码发送失败");
         }
 
-        String code = String.valueOf(ValidateCodeUtils.generateValidateCode(6));
+        //判断是否重复获取验证码
+        String codeInCache = (String) redisTemplate.opsForValue().get(phone);
+        if (StringUtils.isNotEmpty(codeInCache)){
+            log.info(phone+" 5分钟内获取过一次验证码，已响应前端");
+            return R.error("您在5分钟内获取过一次验证码，请勿重复获取");
+        }
 
-        request.getSession().setAttribute(phone, code);
+        //生成验证码
+        String code = String.valueOf(ValidateCodeUtils.generateValidateCode(6));
+        //将验证码存入redis缓存，设置过期时间
+        redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
 
         log.info("你的验证码是：{}", code);
 
@@ -53,23 +67,26 @@ public class UserController {
     public R<User> login(@RequestBody Map phoneAndCode, HttpServletRequest request) {
         String phone = (String) phoneAndCode.get("phone");
         String code = (String) phoneAndCode.get("code");
-
         log.info("接收到用户登录请求，手机号：{}，验证码：{}", phone, code);
 
         //判空，响应失败
-        if (StringUtils.isEmpty(phone) || StringUtils.isNotEmpty(code)) {
+        if (StringUtils.isEmpty(phone) || StringUtils.isEmpty(code)) {
             return R.error("手机号或验证码不能为空");
         }
 
         //验证码核对
-        //错误，响应验证码错误，提示重试
-        if (StringUtils.isEmpty(request.getSession().getAttribute(phone).toString())
-                || !code.equals(request.getSession().getAttribute(phone))) {
-
-            return R.error("验证码错误");
+        //获取缓存中的验证码
+        String codeInCache = (String) redisTemplate.opsForValue().get(phone);
+        if (StringUtils.isEmpty(codeInCache)){//若为空，则可能验证码已过期，或提交的手机号与发送验证码的手机号不一致
+            return R.error("验证码已过期或手机号错误，请检查后重新获取验证码");
         }
 
-        //正确，登录（或注册后登录）
+        //判断，错误：响应验证码错误，提示重试
+        if (!StringUtils.equals(code, codeInCache)) {
+            return R.error("验证码错误，请重试");
+        }
+
+        //正确，登录（或注册后登录），清除缓存中的验证码
 
         //判断是否有该用户，无则注册
         LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
@@ -85,6 +102,9 @@ public class UserController {
 
         //id存入session
         request.getSession().setAttribute("user", user.getId());
+
+        //删除缓存中的验证码
+        redisTemplate.delete(phone);
 
         return R.success(user);
     }
